9994dea1b6b2b486c40e97ac14000c9013a58b75
typsoon <polarny.p@onet.pl>
2024-04-30 15:58:26 +0200

    Refractor

--- core/src/com/bksgames/game/LostInTheMaze.java
+++ core/src/com/bksgames/game/LostInTheMaze.java
@@ -4,12 +4,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.kotcrab.vis.ui.VisUI;
 
-public class Labirynth extends ApplicationAdapter {
+public class LostInTheMaze extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-
+	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
