75948eba44d9d5f2486e96f499f934d3e66a689a
typsoon <polarny.p@onet.pl>
2024-04-30 15:52:41 +0200

    Refractor

--- desktop/src/com/bksgames/game/DesktopLauncher.java
+++ desktop/src/com/bksgames/game/DesktopLauncher.java
@@ -2,14 +2,14 @@ package com.bksgames.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.bksgames.game.Labirynth;
+import com.bksgames.game.LostInTheMaze;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Lost in The Labirynth");
-		new Lwjgl3Application(new Labirynth(), config);
+		config.setTitle("Lost in The Maze");
+		new Lwjgl3Application(new LostInTheMaze(), config);
 	}
 }
--- html/src/com/bksgames/game/client/HtmlLauncher.java
+++ html/src/com/bksgames/game/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.bksgames.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.bksgames.game.Labirynth;
+import com.bksgames.game.LostInTheMaze;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -17,6 +17,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new Labirynth();
+                return new LostInTheMaze();
         }
 }
