e39ba069de89bd9ba1b2235a12afcb40ff2e07df
typsoon <polarny.p@onet.pl>
2024-05-04 19:48:18 +0200

    Testing something

--- core/src/com/bksgames/game/views/GameScreen.java
+++ core/src/com/bksgames/game/views/GameScreen.java
@@ -1,9 +1,11 @@
 package com.bksgames.game.views;
 
+import com.badlogic.gdx.maps.Map;
+
 public class GameScreen {
     final PlayerView playerView;
 
-
+    Map map;
 
     GameScreen(PlayerView playerView) {this.playerView = playerView;}
 }
