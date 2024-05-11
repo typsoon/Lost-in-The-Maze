b07a54654c749db3a6e9ac119e6c7e0096fd3ca4
jajko <madziakycler@gmail.com>
2024-05-11 21:27:29 +0200

    with ifoza

--- core/src/com/bksgames/game/core/Mirror.java
+++ core/src/com/bksgames/game/core/Mirror.java
@@ -1,5 +1,6 @@
 package com.bksgames.game.core;
 
+import com.bksgames.game.enums.Direction;
 import com.bksgames.game.enums.PlayerColor;
 
 public class Mirror{
@@ -8,5 +9,26 @@ public class Mirror{
 
 	enum Orientation {
 		SLASH, BACKSLASH;
+	}
+
+	Direction deflect(Direction direction){
+		Direction afterDirection = direction;
+
+		if(orientation==Orientation.SLASH) {
+//			if(direction==Direction.RIGHT) afterDirection = Direction.UP;
+//			if(direction==Direction.UP) afterDirection = Direction.LEFT;
+//			if(direction==Direction.LEFT) afterDirection = Direction.DOWN;
+//			if(direction==Direction.DOWN) afterDirection = Direction.RIGHT;
+
+			return Direction.values()[(direction.ordinal()-1)%4];
+		}
+		else{
+//			if(direction==Direction.RIGHT) afterDirection = Direction.DOWN;
+//			if(direction==Direction.DOWN) afterDirection = Direction.LEFT;
+//			if(direction==Direction.UP) afterDirection = Direction.RIGHT;
+//			if(direction==Direction.LEFT) afterDirection = Direction.UP;
+
+			return Direction.values()[(direction.ordinal()+1)%4];
+		}
 	}
 }
