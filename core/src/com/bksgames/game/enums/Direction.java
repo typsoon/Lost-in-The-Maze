694e963da280839e6e958c23a45247e039561b47
jajko <madziakycler@gmail.com>
2024-05-11 21:46:52 +0200

    Direction getNext()

--- core/src/com/bksgames/game/enums/Direction.java
+++ core/src/com/bksgames/game/enums/Direction.java
@@ -1,5 +1,17 @@
 package com.bksgames.game.enums;
 
+import java.awt.*;
+
 public enum Direction {
-    LEFT, RIGHT, UP, DOWN
+    LEFT, RIGHT, UP, DOWN;
+
+    static Point getNext(Point point, Direction direction){
+        switch (direction){
+            case LEFT -> point.x--;
+            case RIGHT -> point.x++;
+            case UP -> point.y++;
+            case DOWN -> point.y--;
+        }
+        return point;
+    }
 }
