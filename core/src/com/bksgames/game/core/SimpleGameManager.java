5b38a08cec5006291a95b98a865a00df3093fa75
adamb <adam.burszka@student.tcs.uj.edu.pl>
2024-05-11 21:09:59 +0200

    small changes

--- core/src/com/bksgames/game/core/SimpleGameManager.java
+++ core/src/com/bksgames/game/core/SimpleGameManager.java
@@ -1,15 +1,17 @@
 package com.bksgames.game.core;
 
 import com.bksgames.game.core.boards.Board;
-import com.bksgames.game.core.boards.SquareBoard;
 import com.bksgames.game.core.boards.SquareBoardFactory;
 import com.bksgames.game.enums.PlayerColor;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SimpleGameManager implements GameManager {
     private final Board board;
+    private final Map<PlayerColor, Player> players;
     @Override
     public Boolean makeMove(Move move) {
         return null;
@@ -21,5 +23,8 @@ public class SimpleGameManager implements GameManager {
     }
     public SimpleGameManager(Parameters parameters) {
         this.board = SquareBoardFactory.CreateSBFor2Players(parameters);
+        players = new HashMap<>();
+     //   players.put();
+     //   players.put();
     }
 }
