a865917855fdabf570b2686a0ccac241279d6e17
jajko <madziakycler@gmail.com>
2024-05-11 21:28:13 +0200

    without ifoza

--- core/src/com/bksgames/game/core/Mirror.java
+++ core/src/com/bksgames/game/core/Mirror.java
@@ -15,19 +15,9 @@ public class Mirror{
 		Direction afterDirection = direction;
 
 		if(orientation==Orientation.SLASH) {
-//			if(direction==Direction.RIGHT) afterDirection = Direction.UP;
-//			if(direction==Direction.UP) afterDirection = Direction.LEFT;
-//			if(direction==Direction.LEFT) afterDirection = Direction.DOWN;
-//			if(direction==Direction.DOWN) afterDirection = Direction.RIGHT;
-
 			return Direction.values()[(direction.ordinal()-1)%4];
 		}
 		else{
-//			if(direction==Direction.RIGHT) afterDirection = Direction.DOWN;
-//			if(direction==Direction.DOWN) afterDirection = Direction.LEFT;
-//			if(direction==Direction.UP) afterDirection = Direction.RIGHT;
-//			if(direction==Direction.LEFT) afterDirection = Direction.UP;
-
 			return Direction.values()[(direction.ordinal()+1)%4];
 		}
 	}
