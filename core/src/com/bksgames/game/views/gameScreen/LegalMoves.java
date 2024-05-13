d2079a99bc00e42170ceccdb1124438374390a9c
typsoon <polarny.p@onet.pl>
2024-05-13 15:02:58 +0200

    Fixed the issue temporarily

--- core/src/com/bksgames/game/views/gameScreen/LegalMoves.java
+++ core/src/com/bksgames/game/views/gameScreen/LegalMoves.java
@@ -12,7 +12,6 @@ import com.bksgames.game.globalClasses.enums.MoveTypes;
 import com.bksgames.game.viewmodels.moves.MinionMoveListener;
 import com.bksgames.game.views.gameScreen.actionButtons.ActionButtonFactory;
 
-import javax.swing.text.TableView;
 import java.awt.*;
 import java.util.*;
 
@@ -22,6 +21,9 @@ public class LegalMoves extends Stage {
     private TextureAtlas atlas;
     private final MinionMoveListener minionMoveListener;
 
+    private final Table arrowTable;
+    private final Table actionsTable;
+
     private final ActionButtonFactory factory;
 
     public void displayLegalMoves(Collection<Move> legalMoves, Point minionLocation) {
@@ -29,41 +31,57 @@ public class LegalMoves extends Stage {
 //        TODO: remove code coupling
         minionMoveListener.setLocation(minionLocation);
         setLegalMoves(legalMoves);
-
-        super.setDebugAll(true);
-        super.draw();
+        mainTable.setVisible(true);
     }
 
     private void setLegalMoves(Collection<Move> legalMoves) {
-
+        actionsTable.setVisible(true);
+        actionsTable.setVisible(true);
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (!mainTable.isVisible()) {
+            return false;
+        }
+
         boolean result = super.touchDown(screenX, screenY, pointer, button);
 
-        displayLegalMoves(null, null);
+        mainTable.setVisible(false);
+
         return result;
+    }
+
+    public void deactivateLegalMoves() {
+        mainTable.setVisible(false);
     }
 
     LegalMoves(MinionMoveListener minionMoveListener, TextureAtlas atlas, Camera gameCamera) {
 //        Stage
+        super(new ScreenViewport(gameCamera));
+        super.getViewport().setCamera(gameCamera);
+
         this.minionMoveListener = minionMoveListener;
 
-        super.getRoot().setColor(0,0,0,0);
-        super.setViewport(new ScreenViewport(gameCamera));
+        super.getRoot().setColor(0,0,0,1);
+//        super.setViewport(new ScreenViewport(gameCamera));
+        super.getBatch().setProjectionMatrix(gameCamera.combined);
 
-        mainTable = new Table();
-        mainTable.align(Align.right);
-        this.addActor(mainTable);
-
-        Table arrowTable = new Table();
-        arrowTable.align(Align.bottom);
-
-        Table actionsTable = new Table();
-        actionsTable.align(Align.top);
+//        super.setDebugAll(true);
 
         factory = new ActionButtonFactory(minionMoveListener, atlas);
+
+        mainTable = new Table();
+        mainTable.setPosition((MazeMapFactory.maxBoardWidth+ 10)*MazeMapFactory.tilePixelSize, (MazeMapFactory.maxBoardHeight)*MazeMapFactory.tilePixelSize);
+        mainTable.align(Align.right);
+        this.addActor(mainTable);
+        mainTable.setVisible(false);
+
+        arrowTable = new Table();
+        arrowTable.align(Align.bottom);
+
+        actionsTable = new Table();
+        actionsTable.align(Align.top);
 
         mapping.put(MoveTypes.MOVE, arrowTable);
 
@@ -72,11 +90,21 @@ public class LegalMoves extends Stage {
         mapping.put(MoveTypes.SWORD, actionsTable);
         mapping.put(MoveTypes.MIRROR, actionsTable);
 
+        arrowTable.add(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.UP)));
 
+        arrowTable.row();
 
-        arrowTable.addActor(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.LEFT)));
-        arrowTable.addActor(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.RIGHT)));
-        arrowTable.addActor(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.UP)));
-        arrowTable.addActor(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.DOWN)));
+        arrowTable.add(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.LEFT)));
+        arrowTable.add(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.RIGHT)));
+        arrowTable.add(factory.getButton(new Move(0, 0, MoveTypes.MOVE, Direction.DOWN)));
+
+        mainTable.addActor(actionsTable);
+
+        mainTable.row();
+        mainTable.addActor(arrowTable);
+        arrowTable.debugAll();
+
+        super.act();
+        super.draw();
     }
 }
