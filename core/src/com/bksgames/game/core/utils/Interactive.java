ba508c92e02b3e03777be1411e2d8b30ced21403
adamb <adam.burszka@student.tcs.uj.edu.pl>
2024-05-27 11:02:32 +0200

    small changes

--- core/src/com/bksgames/game/core/utils/Interactive.java
+++ core/src/com/bksgames/game/core/utils/Interactive.java
@@ -3,11 +3,24 @@ package com.bksgames.game.core.utils;
 import com.bksgames.game.common.moves.ActionToken;
 
 /**
- *
- *
+ * Players can interact with
+ * @author riper
  */
 public interface Interactive {
-    int getActionPoints();
-    boolean makeAction(ActionToken actionToken);
-    boolean canMakeAction(ActionToken actionToken);
+ /**
+  * @return amount of active points
+  */
+     int getActionPoints();
+ /**
+  * Make action and use action points
+  * @param actionToken type of action
+  * @return if action was made
+  */
+     boolean makeAction(ActionToken actionToken);
+ /**
+  *
+  * @param actionToken type of action
+  * @return if it can make action
+  */
+     boolean canMakeAction(ActionToken actionToken);
 }
