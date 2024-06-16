79cdd15f5c56c858ce73a52aed020e90433a0b90
RIPer <adam.burszka@student.uj.edu.pl>
2024-06-16 19:06:31 +0200

    cleaner DamageManager

--- core/src/com/bksgames/game/core/main/DamageManager.java
+++ core/src/com/bksgames/game/core/main/DamageManager.java
@@ -10,41 +10,22 @@ import com.bksgames.game.core.utils.Vulnerable;
 import java.util.*;
 
 public class DamageManager {
-    //    Map<Point, Collection<Vulnerable>> receivers;
+
     final Map<Respawnable, Integer> respawnWaitingRoom;
     final GameManager gameManager;
 
-    //    public DamageManager subscribe(Vulnerable vulnerable) {
-//        if(!receivers.containsKey(vulnerable.getPosition())) {
-//            receivers.put(vulnerable.getPosition(), new ArrayList<Vulnerable>());
-//        }
-//        receivers.get(vulnerable.getPosition()).add(vulnerable);
-//        return this;
-//    }
-//    public boolean unsubscribe(Vulnerable vulnerable) {
-//        receivers.get(vulnerable.getPosition()).remove(vulnerable);
-//        return true;
-//    }
     public void dealDamage(SourceOfDamage sourceOfDamage, Collection<Point> range) {
         for (Point point : range) {
-            //if(receivers.containsKey(point))  {
-            //Collection<Vulnerable> dead = new ArrayList<>();
             for (Vulnerable vulnerable : gameManager.getBoard().getTile(point).getVulnerable()) {
                 UpdateHolder<? extends Update> updateHolder = vulnerable.damage(sourceOfDamage);
                 if (updateHolder != null) {
                     gameManager.sendUpdate(updateHolder);
-                    // dead.add(vulnerable);
-                  //  if (gameManager.getBoard().getTile(point).getTunnel() != null)
-                   //     gameManager.getBoard().getTile(point).getTunnel().removeObject(vulnerable);
-
                     if (vulnerable instanceof Respawnable respawnable)
                         respawnWaitingRoom.put(respawnable, 5);//TODO respawn manager with different respawn queue
 
                 }
 
             }
-//                    for(Vulnerable vulnerable:dead)
-//                        unsubscribe(vulnerable);
         }
         gameManager.getVisionManager().playerVisionUpdate();
     }
@@ -56,7 +37,6 @@ public class DamageManager {
             if (respawnWaitingRoom.get(respawnable) == 0) {
                 alive.add(respawnable);
                 respawn(respawnable);
-                //subscribe(respawnable);
             }
         }
         respawnWaitingRoom.keySet().removeAll(alive);
@@ -64,11 +44,9 @@ public class DamageManager {
 
     private void respawn(Respawnable respawnable) {
         var tile = gameManager.getBoard().getTile(respawnable.getBaseSpawnPosition()).getTunnel();
-        //noinspection StatementWithEmptyBody
         if (tile.getTunnel() != null
                 && tile.getEntities().isEmpty()) {
             gameManager.sendUpdate(respawnable.spawn());
-            //tile.addObject(respawnable);
         } else {
             //TODO search for spot
         }
@@ -76,7 +54,6 @@ public class DamageManager {
 
     DamageManager(GameManager gameManager) {
         this.gameManager = gameManager;
-//        receivers = new HashMap<>();
         respawnWaitingRoom = new HashMap<>();
     }
 }
